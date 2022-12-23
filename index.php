75ace85b24ba91122647cd428349081598e54a7d
Ivan <dev@fedresurs.online>
2022-12-23 10:39:03 +0300

    change APIB24

--- index.php
+++ index.php
@@ -11,19 +11,22 @@ function send2B24($answers, $phone) {
         $text .= "\n";
     }
 
-    $text .= "Телефон: $phone";
+    header("Conten-Type: application/json;charset=utf-8");
     $params = [
         "fields" => [
-            "UF_CRM_1670995676556" => $text,
-            "CATEGORY_ID" => "37",
-            "TITLE" => "Заявка с ТГ-Бота",
+            // "UF_CRM_1670995676556" => $text,
+            // "CATEGORY_ID" => "37",
+            "TITLE" => "Заявка с fedresursonline_bot",
             "OPPORTUNITY" => "0",
-            "UTM_CAMPAIGN" => "TG_Bot",
-            // "ASSIGNED_BY_ID" => "118745" // Ответственный!
+            "UTM_CAMPAIGN" => "fedresursonline_bot",
+            "PHONE" => [ [ "VALUE" => $phone, "VALUE_TYPE" => "WORK" ] ],
+            "COMMENTS" => $text,
+            "ASSIGNED_BY_ID" => "1" // Ответственный!
         ],
     ];
 
-    return CRest::call("crm.deal.add", $params);
+    $result = CRest::call("crm.lead.add", $params);
+    echo json_encode( [ 'result' => $result ] );
 }
 
 $data = $_POST;
